85f5f287e1c122c47fe3fb4c05b45e85c6d21598
Robert Tam <rsh.tam@gmail.com>
2019-08-06 18:33:09 +0100

    Add properties, constructors, getters and setters for Folder class

--- src/main/java/com/codeclan/homework/filingsystem/models/Folder.java
+++ src/main/java/com/codeclan/homework/filingsystem/models/Folder.java
@@ -1,4 +1,52 @@
 package com.codeclan.homework.filingsystem.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+  private Long id;
+  private String title;
+  private List<File> files;
+  private User user;
+
+  public Folder(String title) {
+    this.title = title;
+    this.files = new ArrayList<>();
+  }
+
+  public Folder() {
+
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public List<File> getFiles() {
+    return files;
+  }
+
+  public void setFiles(List<File> files) {
+    this.files = files;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
 }
