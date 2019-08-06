342ddd541e79f8b9768b874675f6a5ae5fe3baed
Robert Tam <rsh.tam@gmail.com>
2019-08-06 18:18:59 +0100

    Add Repository interface and tested via checking psql db

--- src/test/java/com/codeclan/homework/filingsystem/FilingsystemApplicationTests.java
+++ src/test/java/com/codeclan/homework/filingsystem/FilingsystemApplicationTests.java
@@ -1,14 +1,19 @@
 package com.codeclan.homework.filingsystem;
 
 import com.codeclan.homework.filingsystem.models.File;
+import com.codeclan.homework.filingsystem.repositories.FileRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FilingsystemApplicationTests {
+
+	@Autowired
+	FileRepository fileRepository;
 
 	@Test
 	public void contextLoads() {
@@ -17,6 +22,7 @@ public class FilingsystemApplicationTests {
 	@Test
 	public void createFile() {
 		File file = new File("test", ".rb", 20);
+		fileRepository.save(file);
 	}
 
 }
