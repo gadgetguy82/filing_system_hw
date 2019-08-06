d584123581d496cb9b433546e314a82e3b40b94e
Robert Tam <rsh.tam@gmail.com>
2019-08-06 18:09:13 +0100

    Add File properties, constructors and getters and setters

--- src/main/java/com/codeclan/homework/filingsystem/models/File.java
+++ src/main/java/com/codeclan/homework/filingsystem/models/File.java
@@ -1,5 +1,51 @@
 package com.codeclan.homework.filingsystem.models;
 
 public class File {
-  
+  private Long id;
+  private String name;
+  private String extension;
+  private int size;
+//  private Folder folder;
+
+  public File(String name, String extension, int size) {
+    this.name = name;
+    this.extension = extension;
+    this.size = size;
+  }
+
+  public File() {
+    
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getExtension() {
+    return extension;
+  }
+
+  public void setExtension(String extension) {
+    this.extension = extension;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
 }
